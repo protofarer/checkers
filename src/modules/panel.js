31b2fa5a531db3de6cfb6fa3ffcd9fc2db6fcee5
Kenny Baron <kennybaron@fastmail.com>
2022-04-15 21:06:41 -0400

    add panel hr

--- src/modules/panel.js
+++ src/modules/panel.js
@@ -8,10 +8,14 @@ export default class Panel {
 
   draw(ctx) {
     ctx.beginPath();
-
     // WARN hardcoded position
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'rgb(0,0,0,0.5)';
     ctx.strokeRect(805, 0, this.width, this.height);
-    ctx.strokeStyle = 'rgb(0,0,0)';
+
+    // Dividing line between players' respective info subpanels
+    ctx.moveTo(815, 400);
+    ctx.lineTo(815 + this.width - 20, 400);
     ctx.stroke();
   }
 }
