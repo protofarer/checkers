691d63a1f785894aff0692df03812995a4f68c7f
Kenny Baron <kennybaron@fastmail.com>
2022-05-19 22:13:53 -0400

    vite bundle config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig, loadEnv } from 'vite'
+import resolve from 'resolve'
 
 export default defineConfig({
   root: 'src',
   build: {
-    outDir: '../dist'
+    rollupOptions: {
+      input: {
+        a: 'src/menu.js',
+        'b/index': 'src/main.js',
+      },
+      output: 'dist'
+
+    }
   },
   envDir: '../',
   // publicDir: '../public',
