fdc26370071e20baaff8b55f0c3962489cc0e5c2
Kenny Baron <kennybaron@fastmail.com>
2022-05-27 02:04:38 -0400

    feat animated menu header

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import Disc from './game/Disc'
+
 const formEle = document.querySelector('form')
 
 formEle.addEventListener('submit', (e) => {
@@ -24,3 +26,103 @@ function loadHandler() {
   settingsMusic.volume = 0.3
 }
 // CSDR import.meta.env.DEV && options to start game in prod or debug
+
+const canvasLeft = document.querySelector('#leftDisc')
+const ctxLeft = canvasLeft.getContext('2d')
+const canvasRight = document.querySelector('#rightDisc')
+const ctxRight = canvasRight.getContext('2d')
+
+// canvasLeft.style.border = canvasRight.style.border = '1px solid blue'
+
+canvasLeft.width = canvasRight.width = canvasLeft.height = canvasRight.height = 105
+
+const disc1 = new Disc(ctxLeft, 0, 0, {x:1, y:1} , 1)
+disc1.radius = 20
+disc1.isKing = true
+
+const disc2 = new Disc(ctxRight, 0, 0, {x:1, y:1} , 1)
+disc2.radius = 20
+disc2.isKing = true
+
+function createDisc(color=null) {
+  let discColor = color === 'red' ? 2 : 1
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+
+  canvas.width = canvas.height = 105
+  // canvas.style.border = '1px solid red'
+
+  const disc = new Disc(ctx, 0, 0, {x:1, y:1} , discColor)
+  disc.radius = 20
+  disc.isKing = true
+  return { disc, canvas, ctx }
+}
+
+let discs = []
+let ctxs = []
+
+const topRow = document.querySelector('#topDiscs')
+const botRow = document.querySelector('#botDiscs')
+
+// Fill top roqw
+for (let i = 0; i < 5; i++) {
+  const x = createDisc(i % 2 ? 'blac' : 'red')
+  discs.push(x.disc)
+  ctxs.push(x.ctx)
+  topRow.append(x.canvas)
+}
+
+for (let i = 0; i < 5; i++) {
+  const x = createDisc(i % 2 ? 'blac' : 'red')
+  discs.push(x.disc)
+  ctxs.push(x.ctx)
+  topRow.append(x.canvas)
+  botRow.append(x.canvas)
+}
+
+let animateStep = 0
+function drawDAOC() {
+  const canvas = document.querySelector('#animatedText')
+  canvas.width = 380
+  canvas.height = 150
+  // canvas.style.border = '1px dotted green'
+  const ctx = canvas.getContext('2d')
+  
+  ctx.font = 'bold 30px courier'
+  ctx.fillStyle = 'hsl(0, 50%, 50%)'
+  animateStep++
+
+  function drawLetter(char, x, y, phase=0, ) {
+    const colorAngle = Math.floor((animateStep + phase)) % 360
+    ctx.fillStyle = `hsl(${colorAngle}, 100%, 40%)`
+    ctx.fillText(char, x, y)
+  }
+
+  const text = 'Dark Age of Checkmelot'
+  
+  return () => {
+    ctx.save()
+    ctx.scale(1,2)
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] !== ' ') {
+        drawLetter(text[i], i*17, 45, i*20)
+      }
+    }
+    ctx.restore()
+  }
+}
+
+function animate() {
+  ctxLeft.clearRect(0, 0, canvasLeft.width, canvasLeft.height)
+  ctxRight.clearRect(0, 0, canvasRight.width, canvasRight.height)
+  disc1.draw()
+  disc2.draw()
+
+  drawDAOC()()
+
+  ctxs.forEach(c => c.clearRect(0, 0, 105, 105))
+  discs.forEach(d => d.draw())
+
+  requestAnimationFrame(animate)
+}
+animate()
